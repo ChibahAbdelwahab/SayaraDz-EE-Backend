18de836c8178b5258bc8adcaccc9546a0fa05fbe
DELL LATITUDE 5490 <fa_chibah@esi.dz>
2019-03-02 20:56:30 +0100

    for merge

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -147,3 +147,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+# Activate Django-Heroku.
+django_heroku.settings(locals())
