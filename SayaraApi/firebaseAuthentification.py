c6dabb85b1e0402c402161690bf822684ccf07e0
DELL LATITUDE 5490 <fa_chibah@esi.dz>
2019-03-23 16:33:06 +0100

    Custom fields for User Added is_client,is_fabricant, fabricant

--- SayaraApi/firebaseAuthentification.py
+++ SayaraApi/firebaseAuthentification.py
@@ -2,7 +2,6 @@ from rest_framework import authentication
 from django.contrib.auth.models import User
 from rest_framework import exceptions
 
-
 import os
 from django.conf import settings
 
@@ -19,13 +18,13 @@ firebase_admin.initialize_app(cred)
 class FirebaseAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         id_token = request.META.get('HTTP_AUTHORIZATION')
-        #print(request.META.get('HTTP_AUTHORIZATION'))
-        decoded_token= None
+        # print(request.META.get('HTTP_AUTHORIZATION'))
+        decoded_token = None
         try:
             decoded_token = auth.verify_id_token(id_token)
-            #print(decoded_token)
+            # print(decoded_token)
         except Exception as e:
-            print('Exception: ',e)
+            print('Exception: ', e)
             pass
 
         if not id_token or not decoded_token:
@@ -34,7 +33,5 @@ class FirebaseAuthentication(authentication.BaseAuthentication):
         uid = decoded_token.get('uid')
         name = decoded_token.get('name')
         email = decoded_token.get('email')
-        user,bool = User.objects.get_or_create(username=name,email=email,is_client=True)
-        #print(user,bool)
-
+        user, bool = User.objects.get_or_create(username=name, email=email, is_client=True)
         return (user, None)
