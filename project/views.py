5c1fb45fb9378415c6cb28f1ce9747661e9321e6
DELL LATITUDE 5490 <fa_chibah@esi.dz>
2019-04-04 21:59:11 +0100

    Bug fixed removed idMarque

--- project/views.py
+++ project/views.py
@@ -9,7 +9,7 @@ class CustomLoginView(LoginView):
         orginal_response = super().get_response()
         try :
             fabricant = self.user.profile.Fabricant.nomFabricant
-            marqueid = self.user.profile.Fabricant.marqueFabricant.idMarque
+            marqueid = self.user.profile.Fabricant.marqueFabricant.id
             marque = self.user.profile.Fabricant.marqueFabricant.nomMarque
         except:
             fabricant=""
