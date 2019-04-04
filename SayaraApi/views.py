77cd5efba17f9c86331277470e98680de7c74107
DELL LATITUDE 5490 <fa_chibah@esi.dz>
2019-04-04 22:08:14 +0100

    Added import for Q

--- SayaraApi/views.py
+++ SayaraApi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, views
 from rest_framework.response import Response
-
+from django.db.models import Q
 from .models import *
 from .serializers import *
 
