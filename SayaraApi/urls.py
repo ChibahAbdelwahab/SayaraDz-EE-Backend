27e470442c10f3acaeb3d2f5f9a6d13e37c3fecf
AbdelwahabChibah <abdelwahab.chibah@dataimpact.fr>
2019-06-21 14:50:31 +0100

    added user annocne query

--- SayaraApi/urls.py
+++ SayaraApi/urls.py
@@ -68,9 +68,9 @@ urlpatterns += (
 
 urlpatterns += (
     # TODO type as a parameter and annonce goes to same view
+    path('annonce', views.AnnonceListView.as_view(), name='AnnonceUser'),
     path('annonce/occasion', views.AnnnonceOccasionListView.as_view(), name='Annonce'),
     path('annonce/neuf', views.AnnonceNeufListView.as_view(), name='Annonce'),
-    path('annonce', views.AnnonceListView.as_view(), name='AnnonceUser'),
 )
 
 
